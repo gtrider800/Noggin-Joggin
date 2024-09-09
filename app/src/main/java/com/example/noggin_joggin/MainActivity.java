e0c2f20fe850bb530129cec7db9510cd8bc17c8a
Andrew St Aubin <ABStAubin@student.fullsail.edu>
2024-09-08 21:36:20 -0400

    Home screen display and get app to open

--- app/src/main/java/com/example/noggin_joggin/MainActivity.java
+++ app/src/main/java/com/example/noggin_joggin/MainActivity.java
@@ -1,73 +1,13 @@
 package com.example.noggin_joggin;
 
 import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.example.noggin_joggin.databinding.ActivityMainBinding;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content view to your activity_main.xml
         setContentView(R.layout.activity_main);
-
-        // If using ViewBinding, uncomment and initialize it
-        // binding = ActivityMainBinding.inflate(getLayoutInflater());
-        // setContentView(binding.getRoot());
-
-        // Initialize Navigation Controller and AppBarConfiguration
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        // Handle Floating Action Button click if present
-        // binding.fab.setOnClickListener(new View.OnClickListener() {
-        //     @Override
-        //     public void onClick(View view) {
-        //         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                 .setAnchorView(R.id.fab)
-        //                 .setAction("Action", null).show();
-        //     }
-        // });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
 
