bb87f55c521d1b983364a6bdbced9412baffa402
Mason Wilkins <MEWilkins1@student.fullsail.edu>
2024-10-15 12:51:13 -0500

    Replaced drawables with new ones to make sure that the pieces fit together, made sure that there is a limit to pieces used per level of game

--- app/src/main/java/com/example/noggin_joggin/ElectricLinkActivity.java
+++ app/src/main/java/com/example/noggin_joggin/ElectricLinkActivity.java
@@ -13,6 +13,18 @@ public class ElectricLinkActivity extends AppCompatActivity
     private ElectricLinkGame game;
     private GridLayout gameGrid;
     private TextView scoreText;
+    private TextView horizontal_info;
+    private TextView vertical_info;
+    private TextView plus_info;
+    private TextView t_info;
+    private TextView right_t_info;
+    private TextView left_t_info;
+    private TextView opposite_t_info;
+    private TextView right_down_curve_info;
+    private TextView right_upper_curve_info;
+    private TextView beginning_info;
+    private TextView end_info;
+
 
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,11 +33,26 @@ public class ElectricLinkActivity extends AppCompatActivity
 
         gameGrid = findViewById(R.id.game_grid);
         scoreText = findViewById(R.id.score_text);
+        horizontal_info = findViewById(R.id.horizontal_info);
+        vertical_info = findViewById(R.id.vertical_info);
+        plus_info = findViewById(R.id.plus_info);
+        t_info = findViewById(R.id.t_info);
+        right_t_info = findViewById(R.id.right_t_info);
+        left_t_info = findViewById(R.id.left_t_info);
+        opposite_t_info = findViewById(R.id.opposite_t_info);
+        right_down_curve_info = findViewById(R.id.right_down_curve_info);
+        right_upper_curve_info = findViewById(R.id.right_upper_curve_info);
+        beginning_info = findViewById(R.id.beginning_info);
+        end_info = findViewById(R.id.end_info);
+
+
         game = new ElectricLinkGame(4, 4);
         setupGrid();
 
         Button resetButton = findViewById(R.id.reset_button);
         resetButton.setOnClickListener(v -> resetGame());
+
+        updatePieceInfo();
     }
 
     private void setupGrid()
@@ -37,13 +64,14 @@ public class ElectricLinkActivity extends AppCompatActivity
             {
                 ImageView currentView = new ImageView(this);
                 currentView.setImageResource(R.drawable.current_background);
+                currentView.setScaleType(ImageView.ScaleType.FIT_XY);
 
                 GridLayout.LayoutParams params = new GridLayout.LayoutParams(GridLayout.spec(i), GridLayout.spec(j));
 
                 int size = convertToDp();
                 params.width = size;
                 params.height = size;
-                params.setMargins(4, 4, 4, 4);
+                params.setMargins(0, 0, 0, 0);
 
                 currentView.setLayoutParams(params);
                 currentView.setAdjustViewBounds(true);
@@ -52,7 +80,8 @@ public class ElectricLinkActivity extends AppCompatActivity
 
                 currentView.setOnClickListener(v -> {
                     game.rotateCurrent(row, col);
-                    updateCurrentView(currentView, game.getCurrent(row, col));
+                    boolean isCurrent = game.isCurrentConnected(row, col);
+                    updateCurrentView(currentView, game.getCurrent(row, col), isCurrent);
                     updateScore();
                 });
 
@@ -67,23 +96,41 @@ public class ElectricLinkActivity extends AppCompatActivity
         return Math.round(80 * density);
     }
 
-    private void updateCurrentView(ImageView currentView, Current current)
+
+
+    private void updateCurrentView(ImageView currentView, Current current, boolean isCurrent)
     {
-        switch (current.getDirection())
+        if (isCurrent)
         {
-            case 0: currentView.setImageResource(R.drawable.current_plus); break;
-            case 1: currentView.setImageResource(R.drawable.current_c); break;
-            case 2: currentView.setImageResource(R.drawable.current_backwards_c); break;
-            case 3: currentView.setImageResource(R.drawable.current_t); break;
-            case 4: currentView.setImageResource(R.drawable.current_y); break;
-            case 5: currentView.setImageResource(R.drawable.current_horizontal); break;
-            case 6: currentView.setImageResource(R.drawable.current_left_down); break;
-            case 7: currentView.setImageResource(R.drawable.current_left_up); break;
-            case 8: currentView.setImageResource(R.drawable.current_right_down); break;
-            case 9: currentView.setImageResource(R.drawable.current_right_up); break;
-            case 10: currentView.setImageResource(R.drawable.current_vertical); break;
+            currentView.setImageResource(R.drawable.current_active);
+        }
+        else
+        {
+            currentView.setImageResource(getImageResourceForDirection(current.getDirection()));
+        }
+
+        updatePieceInfo();
+    }
+
+    private int getImageResourceForDirection(int direction)
+    {
+        switch (direction)
+        {
+            case 0: return R.drawable.current_beginning;
+            case 1: return R.drawable.current_end;
+            case 2: return R.drawable.current_plus;
+            case 3: return R.drawable.current_t_left;
+            case 4: return R.drawable.current_t;
+            case 5: return R.drawable.current_horizontal;
+            case 6: return R.drawable.current_t_right;
+            case 7: return R.drawable.current_upsidedown_t;
+            case 8: return R.drawable.current_right_down;
+            case 9: return R.drawable.current_right_up;
+            case 10: return R.drawable.current_vertical;
+            default: return R.drawable.current_background;
         }
     }
+
     @SuppressLint("StringFormatInvalid")
     private void updateScore()
     {
@@ -95,5 +142,21 @@ public class ElectricLinkActivity extends AppCompatActivity
         game.resetGame();
         setupGrid();
         updateScore();
+        updatePieceInfo();
+    }
+
+    private void updatePieceInfo()
+    {
+        horizontal_info.setText(getString(R.string.horizontal_left, game.getPieceCount(0)));
+        vertical_info.setText(getString(R.string.vertical_left,game.getPieceCount(1)));
+        plus_info.setText(getString(R.string.plus_left, game.getPieceCount(2)));
+        t_info.setText(getString(R.string.t_left,game.getPieceCount(3)));
+        right_t_info.setText(getString(R.string.right_t_left,game.getPieceCount(4)));
+        left_t_info.setText(getString(R.string.left_t_left,game.getPieceCount(5)));
+        opposite_t_info.setText(getString(R.string.opposite_t_left,game.getPieceCount(6)));
+        right_down_curve_info.setText(getString(R.string.right_down_curve_left,game.getPieceCount(7)));
+        right_upper_curve_info.setText(getString(R.string.right_upper_curve_left,game.getPieceCount(8)));
+        beginning_info.setText(getString(R.string.beginning_left,game.getPieceCount(9)));
+        end_info.setText(getString(R.string.end_left,game.getPieceCount(10)));
     }
 }
